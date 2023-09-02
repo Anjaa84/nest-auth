8bfa22934172da04b7bff79cf1a94888e563269e
ANJANAN MANIKAVASAGAR <anjananmy@gmail.com>
2023-09-02 19:47:54 +0530

    feat:cookie added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,61 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
-@Controller()
+@Controller('api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private jwtService: JwtService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('register')
+  async register(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const saltOrRounds = 10;
+
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+
+    return this.appService.createUser({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Res({passthrough:true})response:Response
+  ) {
+    const user = await this.appService.findUser({ email });
+
+    if (!user) {
+      throw new BadRequestException('Invalid Credentials');
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new BadRequestException('Wrong password');
+    }
+
+
+    const jwt = this.jwtService.signAsync({id:user.id})
+
+    response.cookie('jwt',{httpOnly:true})
+
+    return {message:'Success'};
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entitty';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(data: any): Promise<User> {
+    return this.userRepository.save(data);
+  }
+
+  async findUser(condition: any): Promise<User> {
+    if (condition) {
+      return this.userRepository.findOne({ where: condition });
+    }
   }
 }
